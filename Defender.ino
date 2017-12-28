f5836ede3f17a67ac7517850b35af7083068c6f7
FractionUnknown <34917897+FractionUnknown@users.noreply.github.com>
2017-12-28 16:12:49 +0100

    Initial Setup

---
+++ Defender.ino
@@ -0,0 +1,48 @@
+#include <Arduboy2.h>
+Arduboy ardu;
+
+int gamestate = 0;
+int Playerlife;
+int Level;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+ard.begin();
+ard.setFrameRate(30);
+ard.clear();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(!ard.nextFrame()){
+  return;}
+  ard.clear();
+ 
+
+  switch(gamestate){
+    case 0:
+    //title screen
+      ard.setCursonr(0.0);
+      ard.print("Title")
+      break;
+    case 1: 
+    //Gameplay
+      ard.setCursonr(0.0);
+      ard.print("Gameplay")
+    break;
+    case 2:
+    //Win screen
+      ard.setCursonr(0.0);
+      ard.print("Win Screen")
+      break;
+    case 3:
+    //lose screen
+      ard.setCursonr(0.0);
+      ard.print("Lose Screen")
+      break;
+    }
+ ard.display();
+ 
+}
